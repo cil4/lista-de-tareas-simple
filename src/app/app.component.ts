6df49eb2faddefab4ff6a20e28e791e7238b6341
Marianela <masham1860@gmail.com>
2022-08-24 20:32:39 -0300

    Configuración de css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba-Curso-Angular';
+  title = 'Prueba-Curso-Angular';
+  tareas:any[]=[];
+
+  crear(tarea: string, $event: any){
+    $event.preventDefault(); //para prevenir reloading
+    //agregamos una tarea en el array
+    this.tareas.push(
+    {
+      id: Date.now(), //generamos un id en base al momento actual
+      texto: tarea
+    }
+    );
+    console.log(this.tareas);
+    let miForm = document.getElementById("miForm");
+    (<HTMLFormElement>miForm).reset();
+};
+  eliminarTarea(id:number){
+    console.log(id);
+    this.tareas= this.tareas.filter(tarea => tarea.id !== id);
+  }
 }
